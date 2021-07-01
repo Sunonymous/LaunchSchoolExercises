4151a864e22f7140761843d2753f822ce4a0bc2c
Sunny <thankgoditssunny@live.com>
2021-07-01 14:16:51 -0600

    Added Setting and Settings classes in GameBox

--- RB120/lesson_5/gamebox.rb
+++ RB120/lesson_5/gamebox.rb
@@ -2,8 +2,8 @@
 
 # WATCHFOR
 # Keep an eye on how the Settings Menu/Interface interacts with the Recast/Replace Method
+# If too many settings appear, better to refactor to include categories.
 # TODO
-# Add Settings Class
 
 class Box
   attr_reader :width
@@ -91,7 +91,8 @@ module Display
   def choice_box(message, options)
     option_str = options.join('/')
     box_width = [message.length, option_str.length].max
-    box = Box.new(2, [message, option_str], box_width+4)
+    to_print = options.size > 1 ? [message, option_str] : [message]
+    box = Box.new(2, to_print, box_width+4)
     box.to_s.split("\n").each { |line| puts line.center(width, ' ') }
   end
 
@@ -104,11 +105,11 @@ module Display
 end
 
 module Input
-  def get_nonempty_string(prompt)
-    choice_box(prompt, [''])
+  def get_nonempty_string(message, prompt="\t>> ")
+    choice_box(message, [''])
     loop do
-      print("\t>> ")
-      choice = gets.chomp.downcase
+      print(prompt)
+      choice = gets.chomp
       return choice if !choice.strip.empty?
       puts 'Please enter a valid response.'
     end
@@ -129,6 +130,19 @@ module Input
     recast_to_original_type(options.first, selection)
   end
 
+  def get_within_range(range, prompt="\t>> ")
+    # function limited to integer ranges
+    msg = "Please provide a value between #{range.first} and #{range.last}."
+    choice_box(msg, [''])
+    loop do
+      print(prompt)
+      choice = gets.chomp.downcase
+      cast_options = range.to_a.map(&:to_s)
+      return choice.to_i if cast_options.include?(choice)
+      puts 'Invalid response. Please try again.'
+    end
+  end
+
   def recast_to_original_type(klass, object)
     case klass
     when Integer then object.to_i
@@ -138,13 +152,161 @@ module Input
   end
 end
 
+class Settings
+  include Display, Input
+
+  attr_reader :settings, :catalog, :width
+
+  def initialize(menu_width)
+    @settings = {}
+    @catalog = []
+    @width = menu_width
+    @header_spacing = 2
+    @list_spacing = 2
+  end
+
+  def clear;     @settings.clear;            end
+  def get(name); @settings[name].get;        end
+  def set(name); @settings[name].change_val; end
+
+  def prompt(message)
+    puts "\u21E8 #{message} \u21E6".center(@width)
+  end
+
+  def make(name, display_name, value, range, display_vals=nil)
+    @settings[name] = Setting.new(display_name, value, range, display_vals)
+  end
+
+  def menu
+    loop do
+      system('clear')
+      display_settings
+      choice = get_setting_to_edit
+      break if choice == 0
+      alter(get_from_id(choice))
+    end
+  end
+
+  def display_settings(offer_quit=true)
+    @header_spacing.times { puts }
+    prompt('Settings Menu')
+    @header_spacing.times { puts }
+    @list_spacing.times   { puts }
+    list_all_settings(offer_quit)
+    @list_spacing.times   { puts }
+  end
+
+  def build_settings_catalog
+    ids = 1..settings.size
+    @catalog = ids.zip(settings.keys)
+  end
+
+  def list_all_settings(offer_quit)
+    build_settings_catalog
+    catalog.each do |combo|
+      id = combo.first
+      setting = settings[combo.last]
+      puts "\t\t#{id}. #{setting}"
+    end
+    puts "\n\t\tOr type quit to return to the Main Menu." if offer_quit
+  end
+
+  def get_setting_to_edit
+    options = []
+    catalog.each { |pair| options.push(pair.first) }
+    options.concat(['q', 'quit'])
+    replace_with_choice('Choose a setting to edit:', options)
+  end
+
+  def get_from_id(id)
+    catalog.each do |pair|
+      symbol = pair.last
+      return settings[symbol] if pair.first == id
+    end
+  end
+
+  def get_from_named_options(topic, options)
+    numbers = (1..options.size).to_a
+    puts "(#{topic})".center(width)
+    numbers.each do |num|
+      puts "#{num} - #{options[num - 1]}".center(width)
+    end
+    puts "\n\n"
+    (replace_with_choice('Please select an option from above.', numbers)) - 1
+  end
+
+  def alter(setting)
+    clear_screen
+    display_settings(false)
+    prompt = "\t\t(#{setting.name}) << "
+    if setting.range.is_a?(String)
+      msg = "Enter a new value:"
+      setting.set(get_nonempty_string(msg, prompt))
+    elsif setting.range.is_a?(Range)
+      setting.set(get_within_range(setting.range, prompt))
+    elsif setting.range.is_a?(Array) && !setting.boolean?
+      setting.set(get_from_named_options(setting.name, setting.display_values))
+    elsif setting.range.is_a?(Array) && setting.boolean?
+      setting.set(!(setting.value))
+    end
+  end
+end
+
+class Setting
+  TYPES   = [String, Integer, TrueClass, FalseClass]
+  ONOFF   = ['On', 'Off']
+  YESNO   = ['Yes', 'No']
+
+  attr_reader :range, :type, :value, :display_values
+
+  def initialize(display_name, value, range, display_values=nil)
+    if !TYPES.include?(value.class)
+      raise ArgumentError.new("Invalid Setting Type #{value.class}")
+    end
+    @type           = value.class
+    @value          = value
+    @display_name   = display_name
+    @display_values = display_values
+    @range          = range
+    @string_width   = 40
+  end
+
+  def display_value
+    return @value.to_s unless !!display_values
+    "#{@display_values[range.index(@value)]}"
+  end
+
+  def to_s
+    right_hand = @string_width - @display_name.length
+    "#{@display_name}: #{display_value.rjust(right_hand)}"
+  end
+
+  def name; @display_name; end
+  def get;      @value;                        end
+  def boolean?; @range.to_a.size == 2;         end
+
+  def set(new_val)
+    @value = new_val
+  end
+end
+
 
 class GameBox
   include Display, Input
-  attr_reader :width
+  attr_reader :width, :settings
 
   def initialize(width)
     @width = width
+    @settings = Settings.new(@width)
+    default_settings
+  end
+
+  def default_settings
+    settings.clear
+    settings.make(:console_width, 'Game Width', @width, 50..100)
+    settings.make(:player_1_human, 'Player One is Human', true, [true, false], Setting::YESNO)
+    settings.make(:player_1_name, 'Player 1 Name', 'Player', 'str')
+    settings.make(:bot_difficulty, 'Computer Difficulty', 2, (0..2).to_a, ['Haybale', 'Distracted', 'Challenging'])
   end
 end
 
@@ -152,11 +314,15 @@ debug = true
 
 if debug
   puts "\u2317  Tic Tac Toe \u2317"
+  puts
   t = GameBox.new(75)
   # t.clear_screen
-  # t.choice_box('What would you like to do?', %w(jump run sit))
-  test = t.replace_with_choice('Enter a name below', [5, 6, 7])
-  puts "Class: #{test.class}"
+  # t.settings.menu
+  loop do
+    # main_menu
+    # menu_choice
+    break
+  end
 else
   # real program
 end
