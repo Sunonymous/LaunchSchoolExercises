a2690c5476d253d121c17a81d05229a302ea197c
Sunny <thankgoditssunny@live.com>
2021-10-29 18:46:06 -0600

    refactored echo_server with LS suggestions

--- RB175/echo_server.rb
+++ RB175/echo_server.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 require 'socket'
 
-def parse_query(string)
-  return {} if string.nil?
-  pairs = string.split('&')
-  pairs.map do |pair|
+DEBUG = false
+
+def parse_request(request_line)
+  http_method, path_and_parameters, version = request_line.split
+  path, parameters = path_and_parameters.split('?')
+  parameters = parameters.split('&').map do |pair|
     key, value = pair.split('=')
     [key, value]
   end.to_h
+  [http_method, path, parameters]
 end
-
-DEBUG = false
 
 server = TCPServer.new('localhost', 3003)
 
@@ -18,22 +21,17 @@ loop do
 
   request_line = client.gets
   next if !request_line || request_line =~ /favicon/
-  request_split = request_line.split
-  http_method = request_split[0]
-  path, parameters = request_split[1].split('?')
-  parameters = parse_query(parameters) unless parameters.nil?
-
-  puts request_line
+  method, path, parameters = parse_request(request_line)
 
   client.puts request_line
-  client.puts "The method is #{http_method}." if DEBUG
+  client.puts "The method is #{method}." if DEBUG
   client.puts "The path is #{path}." if DEBUG
   client.puts "The parameters are #{parameters}." if DEBUG
   client.puts "The parsed parameters are #{parameters}." if DEBUG
 
-  number_of_rolls = parameters['rolls'] || '1'
-  dice_sides = parameters['sides'] || '6'
-  number_of_rolls.to_i.times { client.puts rand(dice_sides.to_i) + 1 }
+  number_of_rolls = parameters['rolls'].to_i || 1
+  dice_sides      = parameters['sides'].to_i || 6
+  number_of_rolls.times { client.puts rand(dice_sides) + 1 }
 
   client.close
 end
