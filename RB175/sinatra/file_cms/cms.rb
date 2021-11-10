066d2e723add8fd54759b68eb7b1e4abbceee15d
Sunny <thankgoditssunny@live.com>
2021-11-10 10:07:14 -0700

    created basic index page and introduced placeholder data

--- RB175/sinatra/file_cms/cms.rb
+++ RB175/sinatra/file_cms/cms.rb
@@ -2,8 +2,11 @@
 
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'tilt/erubis'
 configure { set :server, :webrick }
 
 get '/' do
-  'Getting started.'
+  @files = Dir.entries('./data/')
+              .select { |file| File.file?(File.join('./data/', file)) }
+  erb(:index)
 end
