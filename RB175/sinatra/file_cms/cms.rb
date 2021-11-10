f627d0bf8cd2ed1211e135e118594bd0d70b509a
Sunny <thankgoditssunny@live.com>
2021-11-10 10:11:34 -0700

    modified index page to use Ruby dir idiom

--- RB175/sinatra/file_cms/cms.rb
+++ RB175/sinatra/file_cms/cms.rb
@@ -5,8 +5,10 @@ require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 configure { set :server, :webrick }
 
+root = File.expand_path(__dir__)
+
 get '/' do
-  @files = Dir.entries('./data/')
+  @files = Dir.entries(File.expand_path("#{root}/data/"))
               .select { |file| File.file?(File.join('./data/', file)) }
   erb(:index)
 end
