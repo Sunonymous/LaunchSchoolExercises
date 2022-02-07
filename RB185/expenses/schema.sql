5de0fe8f3195b6d6d20a0f758cad9821ae1236e5
Sunny <thankgoditssunny@live.com>
2022-02-07 16:46:23 -0700

    Added auto-creation of schema

--- RB185/expenses/schema.sql
+++ RB185/expenses/schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE expenses (
   id         serial        PRIMARY KEY,
   amount     decimal(6, 2) NOT NULL CHECK (amount > 0.00),
   memo       text          NOT NULL,
-  created_on TIMESTAMP     DEFAULT now()
+  created_on TIMESTAMP
 );
